99fc43dfe033e53147ecf61b9d78c274d97675d5
hoang-nguyen <nguyenduyhoangtb35@gmail.com>
2018-10-10 16:48:49 +0700

    init project

---
+++ app.js
@@ -0,0 +1,37 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const nunjucks = require('nunjucks');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
+
+// view engine setup
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+
+});
+
+module.exports = app;
